4fcc8548d864c6c10631e40d85e5ff87851858c2
Marcelo Braga <marcelobraga.dev@gmail.com>
2022-04-29 23:56:42 -0300

    Atualizacao  pacote mercadolivre

--- app/Http/Controllers/Conferentes/Coletas/StatusController.php
+++ app/Http/Controllers/Conferentes/Coletas/StatusController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers\Conferentes\Coletas;
 
 use App\Models\Pacotes;
+use App\src\Pacotes\Status\Base;
 
 class StatusController
 {
     public function pacotesBase()
     {
+        $status = new Base();
+
         $pacotes = new Pacotes();
-        $pacotes = $pacotes->newQuery()->get();
+        $pacotes = $pacotes->newQuery()
+            ->where('status', '=', $status->getStatus())
+            ->get();
 
         return view('pages.conferente.checkin.pacotes-base', compact('pacotes'));
     }
